3e127944013052c1a8ee9a9694d22d2fd7a7db21
Oleg Lazeba <lazebaoleg@gmail.com>
2021-02-28 22:16:01 +0200

    lesson_5

--- app.py
+++ app.py
@@ -41,6 +41,36 @@ def post_detail(id):
     return render_template('post_detail.html', article=article)
 
 
+@app.route('/posts/<int:id>/delete')
+def post_detete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "Error while deleting article!"
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def post_update(id):
+    article = Article.query.get(id)
+
+    if request.method == "POST":
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "Error while editing article!"
+
+    else:
+        return render_template('post_update.html', article=article)
+
+
 @app.route('/create-article', methods=['POST', 'GET'])
 def create_article():
     if request.method == "POST":
